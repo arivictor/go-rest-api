07012d605c3e7dd331b82371cf65142ff7796e66
Ari Victor <11803028+arivictor@users.noreply.github.com>
2022-04-15 18:30:07 +1000

    update: comment model

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -1,43 +1,12 @@
 package comment
 
-import (
-	"context"
-	"errors"
-)
-
-var (
-	ErrorGetComment = errors.New("could not get comment")
-)
-
 // Comment - a representation of a comment
 type Comment struct {
 	ID     string
-	Slug   string
-	Body   string
-	Author string
+	Slug   string `json:"slug"`
+	Body   string `json:"body"`
+	Author string `json:"author"`
 }
 
-// Store - defines all of the methods that our service needs
-type Store interface {
-	GetComment(ctx context.Context, id string) (Comment, error)
-}
-
-// Service - a service for comments
-type Service struct {
-	Store Store
-}
-
-// NewService - returns a new instance of a comment service
-func NewService(store Store) *Service {
-	return &Service{
-		Store: store,
-	}
-}
-
-func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
-	comment, err := s.Store.GetComment(ctx, id)
-	if err != nil {
-		return Comment{}, ErrorGetComment
-	}
-	return comment, nil
-}
+// Comments - a representation of a slice of comments
+type Comments []Comment
