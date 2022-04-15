f9ab840685ab821f3a81b37dd4be323f859872ed
Ari Victor <11803028+arivictor@users.noreply.github.com>
2022-04-15 18:29:54 +1000

    add: database code

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,15 +2,10 @@ package database
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"os"
-)
-
-var (
-	ErrorCouldNotConnect = errors.New("could not connect to database")
 )
 
 type Database struct {
@@ -19,17 +14,17 @@ type Database struct {
 
 func NewDatabase() (*Database, error) {
 	connectionString := fmt.Sprintf(
-		"host=%s port=%s use=%s dbname=%s password=%s sslmode=%s",
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_USER"),
 		os.Getenv("DB_TABLE"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("SSL_MODE"),
 	)
 	connection, err := sqlx.Connect("postgres", connectionString)
 	if err != nil {
-		return nil, ErrorCouldNotConnect
+		return nil, err
 	}
 	return &Database{Client: connection}, nil
 }
