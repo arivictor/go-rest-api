22245d60d2166323a12fb2521f0d5717a0b05d9d
Ari Victor <11803028+arivictor@users.noreply.github.com>
2022-04-15 18:29:32 +1000

    update: main.go with routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,71 @@
 package main
 
 import (
-	"fmt"
+	"github.com/arivictor/go-api/internal/comment"
+	"github.com/arivictor/go-api/internal/database"
+	"github.com/arivictor/go-api/internal/server"
+	"github.com/arivictor/go-api/internal/transport"
+	"github.com/gorilla/mux"
 	"log"
 )
 
 // Responsible for instantiation of startup of application
 func Run() error {
-	fmt.Println("Hello World")
+	log.Println("Connecting to database...")
+	db, err := database.NewDatabase()
+	if err != nil {
+		return err
+	}
+
+	log.Println("Migrating database...")
+	err = db.MigrateDatabase()
+	if err != nil {
+		return err
+	}
+
+	// Provision router
+	router := mux.NewRouter()
+
+	// Sets up our middleware functions
+	router.Use(transport.JSONMiddleware)
+
+	// we also want to log every incoming request
+	router.Use(transport.LoggingMiddleware)
+
+	// We want to timeout all requests that take longer than 15 seconds
+	router.Use(transport.TimeoutMiddleware)
+
+	// Handles database interactions
+	commentRepository := comment.NewRepository(db)
+
+	// Handles business logic
+	commentService := comment.NewService(commentRepository)
+
+	// Handles HTTP requests
+	commentHandler := comment.NewHandler(commentService)
+
+	// Route request to handlers
+	router.HandleFunc("/api/v1/comment/{id}", commentHandler.Delete).Methods("DELETE")
+	router.HandleFunc("/api/v1/comment/{id}", commentHandler.Update).Methods("PATCH")
+	router.HandleFunc("/api/v1/comment/{id}", commentHandler.Get).Methods("GET")
+	router.HandleFunc("/api/v1/comment", commentHandler.List).Methods("GET")
+	router.HandleFunc("/api/v1/comment", commentHandler.Create).Methods("POST")
+
+	// Provision HTTP server
+	s := server.NewServer("8080", router)
+
+	// Start Server
+	err = s.Serve()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func main() {
 	err := Run()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("error: %v", err)
 	}
 }
